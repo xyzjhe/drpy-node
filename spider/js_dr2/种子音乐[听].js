4b7722f4adf411b405de64918e8fc3b4985e801d
eylinsir <283371717@qq.com>
2026-01-15 15:33:54 +0800

    更新酷我音乐相关文件

--- spider/js_dr2/种子音乐[听].js
+++ spider/js_dr2/种子音乐[听].js
@@ -21,20 +21,35 @@ var rule = {
     },
     class_parse: '.aside-menu-list.channel&&[href*=list];a&&Text;a&&href;.(list.*).htm',
     play_parse: true,
+    
+    // --- 重构的核心部分 ---
     lazy: $js.toString(() => {
+        // 1. 将详情页 URL 替换为 API 接口 URL
         input = input.replace(/play\/(\w+)\.htm/, 'ajax/?act=songinfo&id=$1&lang=');
-        log(input);
-        let mp3 = JSON.parse(request(input)).data.mp3;
-        input = {parse: 0, url: mp3, header: rule.headers};
+        
+        // 2. 发起请求并解析 JSON
+        let html = request(input);
+        let json = JSON.parse(html);
+        let data = json.data;
+        
+        // 3. 构造播放对象，包含歌词和元数据
+        input = {
+            parse: 0,
+            url: data.mp3,          // 播放链接
+            header: rule.headers,   // 请求头
+            lrc: data.lrc,          // 歌词内容
+            img: data.pic,          // 封面图片
+            title: data.mname,      // 歌名
+            singer: data.sname      // 歌手
+        };
     }),
+    // ---------------------
+    
     limit: 6,
-    //  图片来源:'@Referer=https://www.zz123.com/',
     推荐: "*",
     一级: '.mobile-list&&.mobile-list-item;.songname&&Text;.lazyload&&data-src;.authorname&&Text;a&&href',
     二级: '*',
-    // searchUrl:'/search/?key=**&page=fypage',
-    // 搜索:'*',
     searchUrl: '/ajax/?act=search&key=**&lang=',
-    detailUrl: '/play/fyid.html',
+    detailUrl: '/play/fyid.html', // 修正：原来是 play/fyid.html，这里保持一致，lazy中正则会匹配
     搜索: 'json:data;mname;pic;sname;id',
 }
